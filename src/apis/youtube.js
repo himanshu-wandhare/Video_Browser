81f4b897041bbb07c2e9120c4a738feafa09b24a
himanshu-wandhare <himanshuwandhare13@gmail.com>
2023-07-15 20:36:01 +0530

    api update

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
+  headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
   params: {
     part: "snippet",
     type: "video",
